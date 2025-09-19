473dfcd116f26ba83bbe01b396798b4d824fbe09
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-19 14:09:27 +0530

    check divisible

---
+++ is_prime/is_divisible.js
@@ -0,0 +1,7 @@
+let dividend = 9;
+let divisor = 2;
+
+let remainder = dividend % divisor;
+let isDivisible = remainder === 0;
+
+console.log("the no", dividend, "/" , divisor ,"is divisible ?",isDivisible);
