12892fdc0287703bb6fdc076c2e879280191b985
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-19 14:19:04 +0530

    add n no. of number

---
+++ add_n_numbers/add_n_number.js
@@ -0,0 +1,10 @@
+let inputNumber = 7;
+let initialValue = 1;
+let sum = 0;
+
+while (initialValue <= inputNumber) {
+    sum = sum + initialValue;
+    initialValue = initialValue + 1;
+}
+
+console.log("sum of", inputNumber, "number is ", sum);    
