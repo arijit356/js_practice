87c1d22198360a33624d6f7b193ef8797ebd7f9c
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-21 12:43:11 +0530

    switch case experiment

---
+++ string_number/switch_case.js
@@ -0,0 +1,41 @@
+let numberString = "2";
+let number = 0;
+
+switch (numberString) {
+    case "0":
+        number = 0;
+        break;
+    case "1":
+        number = 1;
+        break;
+    case "2":
+        number = 2;
+        break;
+    case "3":
+        number = 3;
+        break;
+    case "4":
+        number = 4;
+        break;
+    case "5":
+        number = 5;
+        break;
+    case "6":
+        number = 6;
+        break;
+    case "7":
+        number = 7;
+        break;
+    case "8":
+        number = 8;
+        break;
+    case "9":
+        number = 9;
+        break;
+
+    default:
+        console.log("no number");
+        break;
+}
+
+console.log(number);
