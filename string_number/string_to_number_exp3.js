a0bcd445d872d5085802f65923b0bc80526af587
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-21 12:40:33 +0530

    string to number experiment 3

---
+++ string_number/string_to_number_exp3.js
@@ -0,0 +1,33 @@
+let string = "234";
+let reminder = 0;
+let number = 0;
+
+number = number * 10;
+reminder = string % 10;
+number = number + reminder;
+string = (string - reminder) / 10;
+
+console.log(reminder);
+console.log(number);
+// console.log(typeof(reminder));
+console.log(string, "\n");
+
+number = number * 10;
+reminder = string % 10;
+number = number + reminder;
+string = (string - reminder) / 10;
+
+console.log(reminder);
+console.log(number);
+// console.log(typeof(reminder));
+console.log(string, "\n");
+
+number = number * 10;
+reminder = string % 10;
+number = number + reminder;
+string = (string - reminder) / 10;
+
+console.log(reminder);
+console.log(number);
+// console.log(typeof(reminder));
+console.log(string, "\n");
