973e7b7cc1ae40bb9217abfc07a92f81d6e265ed
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-21 12:35:43 +0530

    string to number conversion

---
+++ string_number/string_to_number.js
@@ -0,0 +1,10 @@
+let stringNumber = "23";
+let number = 0;
+
+let stringToNumber = number + stringNumber;
+
+console.log(stringToNumber);
+console.log(typeof(stringToNumber));
+
+console.log(typeof(stringNumber));
+console.log(typeof(number));
