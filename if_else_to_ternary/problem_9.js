59c60deb8ccb500d66703fb02df94a6a3af527f6
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-27 15:24:56 +0530

    Update problem_9.js

--- if_else_to_ternary/problem_9.js
+++ if_else_to_ternary/problem_9.js
@@ -1,1 +1,10 @@
+/* rewrite these as ternary forms or remove the else.
 
+if (role === "teacher") {
+  dashboard = "gradebook";
+  permissions = "view/edit/grade";
+} else {
+  dashboard = "courses";
+  permissions = "view";
+}
+*/
