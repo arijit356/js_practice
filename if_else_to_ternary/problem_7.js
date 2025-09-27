828159c36c6d32702c4d2ac0ad3fe51c9cc50c76
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-27 15:24:29 +0530

    Update problem_7.js

--- if_else_to_ternary/problem_7.js
+++ if_else_to_ternary/problem_7.js
@@ -1,1 +1,11 @@
+/* rewrite these as ternary forms or remove the else.
 
+if (isPremiumUser) {
+  discount = basePrice * 0.2;
+  finalPrice = basePrice - discount;
+} else {
+  discount = basePrice * 0.05;
+  finalPrice = basePrice - discount;
+}
+
+*/
