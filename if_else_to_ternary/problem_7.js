078bd3b3307c199d038dd3eab6e2d38f3ee137dc
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-27 15:54:27 +0530

    find discount

--- if_else_to_ternary/problem_7.js
+++ if_else_to_ternary/problem_7.js
@@ -9,3 +9,12 @@ if (isPremiumUser) {
 }
 
 */
+
+const isPremiumUser = true;
+const basePrice = 4;
+
+const discount = isPremiumUser ? basePrice * 0.2 : basePrice * 0.05;
+const finalPrice = basePrice - discount;
+
+console.log(discount);
+console.log(finalPrice);
