2e03d03b85c3b6407ae2e80599d084aa2fd4e8c8
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-27 15:24:11 +0530

    Update problem_6.js

--- if_else_to_ternary/problem_6.js
+++ if_else_to_ternary/problem_6.js
@@ -1,1 +1,11 @@
+/* rewrite these as ternary forms or remove the else.
 
+if (userType === "admin") {
+  baseAccess = "all";
+  canDelete = true;
+} else {
+  baseAccess = "limited";
+  canDelete = false;
+}
+
+*/
