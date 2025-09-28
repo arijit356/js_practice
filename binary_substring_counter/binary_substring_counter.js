e8265e174aa704fa6b9242c58e5a405300013ce9
Arijit Mandal <arijitmandal356@gmail.com>
2025-09-28 11:39:46 +0530

    Update binary_substring_counter.js

--- binary_substring_counter/binary_substring_counter.js
+++ binary_substring_counter/binary_substring_counter.js
@@ -1,6 +1,46 @@
 
-function ocuranseInBinary(decimal, string, substring) {
-  return 1;
+function isDecimalToBinary(decimal) {
+  let mul = 1;
+  let binary = 0;
+
+  let index = 1;
+  while (index <= decimal) {
+
+    let reminder = decimal % 2;
+    binary = binary + (reminder * mul);
+    mul = mul * 10;
+    decimal = (decimal - reminder) / 2;
+  }
+  const binaryString = "" + binary;
+  // console.log(binaryString);
+  return binaryString;
+}
+
+function isSubstring(string, subString, lengthOfSubString, index) {
+  let subStringMatch = true;
+  for (let innerIndex = 0; innerIndex < lengthOfSubString; innerIndex++) {
+
+    if (string[index + innerIndex] !== subString[innerIndex]) {
+      subStringMatch = false;
+    }
+  }
+  return subStringMatch;
+}
+
+function ocuranseInBinary(decimal, subString) {
+  const string = isDecimalToBinary(decimal);
+  const lengthOfString = string.length;//1000001  00
+  const lengthOfSubString = subString.length;
+
+  let count = 0;
+  for (let index = 0; index <= lengthOfString - lengthOfSubString; index++) {
+    let isMatch = isSubstring(string, subString, lengthOfSubString, index);
+
+    if(isMatch){
+      count ++;
+    }
+  }
+  return count;
 }
 
 function getResultSymbol(expectedValue, actualValue) {
@@ -15,26 +55,27 @@ function displayResult(symbol, inputSection, actualSection, expectedSection) {
   console.log(message);
 }
 
-function composeMessage(decimal, string, substring, expectedValue, actualValue) {
+function composeMessage(decimal, substring, expectedValue, actualValue) {
   const symbol = getResultSymbol(expectedValue, actualValue);
   const decimalSection = "decimal number = " + decimal;
-  const inputSection = "string = " + string + ", substring = " + substring;
+  const inputSection = "substring = " + substring + " " + decimalSection;
   const actualSection = "result = " + actualValue;
   const expectedSection = " expected value = " + expectedValue;
-  displayResult(symbol, decimalSection, inputSection, actualSection, expectedSection);
+  displayResult(symbol, inputSection, actualSection, expectedSection);
 }
 
-function testBinarySubstring(decimal, string, substring, expectedValue) {
-  const actualValue = ocuranseInBinary(decimal, string, substring);
-  composeMessage(decimal, string, substring, expectedValue, actualValue);
+function testBinarySubstring(decimal, substring, expectedValue) {
+  const actualValue = ocuranseInBinary(decimal, substring);
+  composeMessage(decimal, substring, expectedValue, actualValue);
 }
 function testAll() {
-  testBinarySubstring(65, "1000001", "10", 1);
-  testBinarySubstring(65, "1000001", "00", 4);
-  testBinarySubstring(21, "10101", "101", 2);
-  testBinarySubstring(31, '11111', '11', 4);
-  testBinarySubstring(8, '1000', '10', 1);
-  testBinarySubstring(19, '10011', '1', 3);
+  testBinarySubstring(65, "00", 4);
+  testBinarySubstring(62, '11', 4);
+  testBinarySubstring(65, "10", 1);
+  testBinarySubstring(21, "101", 2);
+  testBinarySubstring(8, '10', 1);
+  testBinarySubstring(19, '1', 3);
+
 }
 
 testAll();
