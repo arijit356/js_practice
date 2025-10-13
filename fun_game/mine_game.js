a45d3566680f06b5b95a605b3198b709b44925d5
Arijit Mandal <arijitmandal356@gmail.com>
2025-10-13 19:02:40 +0530

    Update mine_game.js

--- fun_game/mine_game.js
+++ fun_game/mine_game.js
@@ -1,4 +1,4 @@
-
+const CORRECT = "✅";
 const BOOM = "💥";
 
 const NUMBER_BOARD = [
@@ -25,14 +25,12 @@ const COPY_NUMBER_BOARD = [
   "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
   "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"
 ];
+const CORRECT_NUMBER = [];
 
 function renderBoard() {
   let boardItems = "";
   for (let index = 0; index < COPY_NUMBER_BOARD.length; index++) {
-    if (COPY_NUMBER_BOARD[index].length < 2) {
-      COPY_NUMBER_BOARD[index] = (COPY_NUMBER_BOARD[index]).padStart(2, " ");
-    }
-    boardItems += COPY_NUMBER_BOARD[index] + " ";
+    boardItems += COPY_NUMBER_BOARD[index] + "\t";
     if (index % 10 === 9) {
       boardItems += "\n"
     }
@@ -44,11 +42,11 @@ function randomNumberGenerator(endRange) {
   return Math.floor((Math.random() * endRange) + 1);
 }
 
-function play() {
+function booms() {
   let boomIndex = [];
-  for (let index = 0; index < 15; index++) {
+  for (let index = 0; index <= 20; index++) {
     let randomNumber = randomNumberGenerator(NUMBER_BOARD.length);
-    if(NUMBER_BOARD[randomNumber] !== BOOM){
+    if (NUMBER_BOARD[randomNumber] !== BOOM) {
       NUMBER_BOARD[randomNumber] = BOOM;
       boomIndex.push(randomNumber);
     }
@@ -56,10 +54,45 @@ function play() {
   return boomIndex;
 }
 
-function main() {
+function isLossGame(userInput, IndexOfBooms) {
+  console.log(IndexOfBooms);
+
+  if (IndexOfBooms.includes(userInput)) {
+    COPY_NUMBER_BOARD[userInput - 1] = BOOM;
+    return true;
+  }
+  CORRECT_NUMBER.push(userInput);
+  COPY_NUMBER_BOARD[userInput - 1] = CORRECT;
+  return false;
+}
+
+function play(indexOfBooms, count) {
+  const userInput = parseInt(prompt("enter a number : "));
+  if (userInput < 1 || userInput > 100 || isNaN(userInput) || CORRECT_NUMBER.includes(userInput)) {
+    console.log("Invalid input, give proper inputs with in range : ");
+    return play(indexOfBooms);
+  }
+
+  if (isLossGame(userInput, indexOfBooms)) {
+    console.clear();
+    console.log(renderBoard());
+    console.log("you loss the game");
+    return;
+  }
+  console.clear();
   console.log(renderBoard());
-  console.log(NUMBER_BOARD[3]);
-  play();
+  count++;
+  if (count === NUMBER_BOARD.length - indexOfBooms.length) {
+    console.log("you won the match");
+  }
+  return play(indexOfBooms);
+}
+
+function main() {
+  let indexOfBooms = booms();
+  console.log(`There are ${indexOfBooms.length} booms...`);
+  let count = 0;
+  play(indexOfBooms, count);
 }
 
 main();
