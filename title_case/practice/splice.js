03a8ab306e672e66e7ce4a436a476dc95752f323
Arijit Mandal <arijitmandal356@gmail.com>
2025-10-19 13:02:48 +0530

    Create splice.js

---
+++ title_case/practice/splice.js
@@ -0,0 +1,6 @@
+    let myArray = ["a","p","p","l","e"];
+    let indexToRemove = 0; 
+
+    myArray.splice(indexToRemove, 1); // Removes 1 element starting at index 2
+    console.log(myArray); 
+    console.log(myArray.length);
