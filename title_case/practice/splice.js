b23e7b9beabfd78df8fe4768ae6bf94cbfc7726d
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-03 15:07:33 +0530

    Delete title_case directory

--- title_case/practice/splice.js
+++
@@ -1,6 +0,0 @@
-    let myArray = ["a","p","p","l","e"];
-    let indexToRemove = 0; 
-
-    myArray.splice(indexToRemove, 1); // Removes 1 element starting at index 2
-    console.log(myArray); 
-    console.log(myArray.length);
