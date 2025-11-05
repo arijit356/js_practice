8b13d595089072bd7e583ec2883bd4d6f5d8fb59
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-05 18:57:52 +0530

    Create reverse_of_binary_number.js

---
+++ practice/decimal_to_binary/reverse_of_binary_number.js
@@ -0,0 +1,7 @@
+let binary = "0001";
+let reverseBinaryString = "";
+
+for(let i = 1; i<= binary.length; i++){
+    reverseBinaryString = reverseBinaryString + binary[(binary.length -i)];
+}
+console.log(reverseBinaryString);
