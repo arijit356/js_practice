057f185508abb63e88f75fd91d41df1c8c67c093
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-05 18:16:13 +0530

    Update sorting_anonymus.js

--- practice/sorting_anonymus.js
+++ practice/sorting_anonymus.js
@@ -1,43 +1,44 @@
-const DATA = [74, 7, 100, 106, 0, 12, 31, 28, 92, 67, 94, 35, 45, 14, 100, 122, 82, 34, 56, 52, 12, 32, 91, 32, 8, 7, 15, 62, 34, 88, 7, 100, 23, 80, 27, 22, 14, 67, 61, 20, 52, 41, 8, 100, 62, 88, 34, 10, 41, 100, 8, 34, 7, 100, 9, 17, 34, 52, 100, 34, 0, 100, 17, 34, 52, 100, 34, 0, 100, 17, 34, 52, 100, 34, 0, 100, 17, 34, 52, 100, 34, 0, 100, 17, 34, 52, 100, 34, 0, 100];
-// const DATA = [0, 0, 0, 1, 1, 1, 2];
+// function lessThen(x, y) {
+//   return x.length < y.length;
+// }
 
-function sort(data) {
-  const dataToSort = data.slice();
-  for (let i = 0; i < dataToSort.length; i++) {
-    for (let j = i + 1; j < dataToSort.length; j++) {
-      if (dataToSort[i] > dataToSort[j]) {
-        let temp = dataToSort[i];
-        dataToSort[i] = dataToSort[j];
-        dataToSort[j] = temp;
+const isString = function (value) {
+  return typeof value === "string";
+}
+
+function sort(data, lessThen) {
+  const sorted = data.slice();
+  for (let i = 0; i < sorted.length; i++) {
+    for (let j = i + 1; j < sorted.length; j++) {
+      if (lessThen(sorted[i], sorted[j])) {
+        const temp = sorted[i];
+        sorted[i] = sorted[j];
+        sorted[j] = temp;
       }
     }
   }
-  return dataToSort;
+  return sorted;
 }
 
-function modeOf(sortedData) {
-  let count = 1;
-  let mostFrequentNum = 0;
-  let mostFrequentElement = [];
-  for (let index = 0; index < sortedData.length; index++) {
-    count++;
-    if (sortedData[index] !== sortedData[index + 1]) {
-      mostFrequentNum = mostFrequentNum < count ? count: mostFrequentNum;
-      if (mostFrequentNum === count) {
-        mostFrequentElement.push(sortedData[index]);
-      }
-      count = 1;
-      continue;
-    }
-  }
-  console.log(mostFrequentElement);
-}
+const isLessThen = function (firstValue, secondValue) {
+  firstValue = isString(firstValue) ? firstValue.length : firstValue;
+  secondValue = isString(secondValue) ? secondValue.length : secondValue;
+
+  return firstValue < secondValue;
+};
 
 function main() {
-  const sortedItems = sort(DATA);
-  const mostFrequencyValues = modeOf(sortedItems);
-  console.log(sortedItems)
-  console.log(mostFrequencyValues);
+  const number = [5, 2, 4, 1, 3];
+  const string = ["aaa", "aa", "aaaa", "avhgfr", "ab", "a"];
+  const stringNumber = ["aaa", "aa", "aaaa", 11, 45, "avhgfr", "ab", 1];
+  const sortedData1 = sort(number, isLessThen).join(" ");
+  const sortedData2 = sort(string, isLessThen).join(" ");
+  const sortedData3 = sort(stringNumber, isLessThen).join(" ");
+
+  console.log(sortedData1);
+  console.log(sortedData2);
+  console.log(sortedData3);
+
 }
 
 main();
