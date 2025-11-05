7f69a5df5e65022fcaefb14e2d8bc34c6a374748
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-05 19:02:11 +0530

    Create factorial_exp2.js

---
+++ practice/factorial/factorial_exp2.js
@@ -0,0 +1,29 @@
+const inputNumber = 1;
+let initialValue = 1; 
+let factorialOfInputNumber = 1; 
+
+
+factorialOfInputNumber = factorialOfInputNumber * initialValue; 
+initialValue = initialValue + 1;
+
+console.log(inputNumber , "no factorial is 1", factorialOfInputNumber );
+
+factorialOfInputNumber = factorialOfInputNumber * initialValue; 
+initialValue = initialValue + 1;
+
+console.log(inputNumber , "no factorial is 1", factorialOfInputNumber );
+
+factorialOfInputNumber = factorialOfInputNumber * initialValue; 
+initialValue = initialValue + 1;
+
+console.log(inputNumber , "no factorial is 1", factorialOfInputNumber );
+
+factorialOfInputNumber = factorialOfInputNumber * initialValue; 
+initialValue = initialValue + 1;
+
+console.log(inputNumber , "no factorial is 1", factorialOfInputNumber );
+
+factorialOfInputNumber = factorialOfInputNumber * initialValue; 
+initialValue = initialValue + 1;
+
+console.log(inputNumber , "no factorial is 1", factorialOfInputNumber );
