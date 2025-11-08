540d126d6fc0ad56c5a353397e747c0058d90e08
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-08 21:37:38 +0530

    Create reduce.js

---
+++ practice/inbuild_function/reduce.js
@@ -0,0 +1,69 @@
+const productAllNumbers = (number1, number2)=>{
+return number1 * number2;
+}
+const concatStrings = (str1,str2)=>{
+  return str1.concat(str2);
+}
+const reduce = function (array, reducer,initialValue) {
+  let result = initialValue;
+  for (let index = 0; index < array.length; index++) {
+    result = reducer(result,array[index]);
+  }
+  return result;
+}
+
+function areDeepEqual(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let index = 0; index < array1.length; index++) {
+    if (!areEqual(array1[index], array2[index])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function areEqual(firstValue, secondValue) {
+  if (Array.isArray(firstValue) && Array.isArray(secondValue)) {
+    return areDeepEqual(firstValue, secondValue);
+  }
+  return firstValue === secondValue;
+}
+
+function displayResult(input, actual, expected, purpose) {
+  let message = "❌";
+  message += " " + purpose;
+  message += "\n\n | " + input;
+  message += "\n | " + actual;
+  message += "\n | " + expected;
+  message += "\n" + "--------------- \n";
+  return message;
+}
+
+function composeMessage(text, expectedValue, actualValue, purpose) {
+  const input = `array = [${text}]`;
+  const actual = `result = [${actualValue}]`;
+  const expected = `expected value = [${expectedValue}]`;
+
+  if (areEqual(expectedValue, actualValue)) {
+    return `✅ ${purpose}`;
+  }
+  return displayResult(input, actual, expected, purpose);
+}
+
+const testOperation = function (data, expectedValue, purpose, operation, functionToUse, initialValue) {
+  const actualValue = functionToUse(data, operation, initialValue);
+  console.log(
+    composeMessage(data, expectedValue, actualValue, purpose),
+  );
+}
+
+function testAll() {
+   testOperation([1, 2, 3, 4], 24, "product of numbers", productAllNumbers, reduce,1);
+  testOperation(["ab", "so", "lu", "te"], "absolute", "combine strings", concatStrings, reduce,"");
+
+
+}
+
+testAll();
