308383b1003eb04d08ecd1cb6f9ef931340c111c
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-08 22:36:30 +0530

    Create some.js

---
+++ practice/inbuild_function/some.js
@@ -0,0 +1,66 @@
+const isOddNumber = (number) => {
+  return number % 2 !== 0;
+}
+const some = (array, predicate) => {
+  for (const element of array) {
+    if (predicate(element)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const areDeepEqual = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let index = 0; index < array1.length; index++) {
+    if (!areEqual(array1[index], array2[index])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const areEqual = (firstValue, secondValue) => {
+  if (Array.isArray(firstValue) && Array.isArray(secondValue)) {
+    return areDeepEqual(firstValue, secondValue);
+  }
+  return firstValue === secondValue;
+}
+
+const displayResult = (input, actual, expected, purpose) => {
+  let message = "❌";
+  message += " " + purpose;
+  message += "\n\n | " + input;
+  message += "\n | " + actual;
+  message += "\n | " + expected;
+  message += "\n" + "--------------- \n";
+  return message;
+}
+
+const composeMessage = (text, expectedValue, actualValue, purpose) => {
+  const input = `array = [${text}]`;
+  const actual = `result = [${actualValue}]`;
+  const expected = `expected value = [${expectedValue}]`;
+
+  if (areEqual(expectedValue, actualValue)) {
+    return `✅ ${purpose}`;
+  }
+  return displayResult(input, actual, expected, purpose);
+}
+
+const testOperation = (data, expectedValue, purpose, operation, functionToUse, initialValue) => {
+  const actualValue = functionToUse(data, operation, initialValue);
+  console.log(
+    composeMessage(data, expectedValue, actualValue, purpose),
+  );
+}
+
+function testAll() {
+  testOperation([1, 2, 3, 4], true, "at least one odd numbers", isOddNumber, some);
+  testOperation([2, 4], false, "all even numbers", isOddNumber, some);
+
+}
+
+testAll();
