4596e290e715dc37888f62b3b1d5d3e00f11626a
Arijit Mandal <arijitmandal356@gmail.com>
2025-11-08 21:09:31 +0530

    Create map.js

---
+++ practice/inbuild_function/map.js
@@ -0,0 +1,65 @@
+const squareRoots = function (number){
+  return Math.sqrt(number);
+}
+
+const map = function (array, mapper){
+  const result = [];
+  for (let index = 0; index < array.length; index++) {
+    result.push(mapper(array[index]))
+  }
+  return result;
+}
+
+function areDeepEqual(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let index = 0; index < array1.length; index++) {
+    if (!areEqual(array1[index], array2[index])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function areEqual(firstValue, secondValue) {
+  if (Array.isArray(firstValue) && Array.isArray(secondValue)) {
+    return areDeepEqual(firstValue, secondValue);
+  }
+  return firstValue === secondValue;
+}
+
+function displayResult(input, actual, expected, purpose) {
+  let message = "❌";
+  message += " " + purpose;
+  message += "\n\n | " + input;
+  message += "\n | " + actual;
+  message += "\n | " + expected;
+  message += "\n" + "--------------- \n";
+  return message;
+}
+
+function composeMessage(text, expectedValue, actualValue, purpose) {
+  const input = `array = [${text}]`;
+  const actual = `result = [${actualValue}]`;
+  const expected = `expected value = [${expectedValue}]`;
+
+  if (areEqual(expectedValue, actualValue)) {
+    return `✅ ${purpose}`;
+  }
+  return displayResult(input, actual, expected, purpose);
+}
+
+const testOperation = function (data, expectedValue, purpose, operation, functionToUse,initialValue) {
+  const actualValue = functionToUse(data, operation,initialValue);
+  console.log(
+    composeMessage(data, expectedValue, actualValue, purpose),
+  );
+}
+
+function testAll() {
+  testOperation([1, 4, 9, 16], [1, 2, 3, 4], "square root of values", squareRoots, map);
+
+}
+
+testAll();
